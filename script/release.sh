78de5c1dca4bff82b35b4e76ec5b3d940fc29ce2
Tony Abboud <tabboud@palantir.com>
2024-04-24 18:23:45 -0400

    cr

--- script/release.sh
+++ script/release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -uo pipefail
+set -euo pipefail
 
 main() {
   : "${GH_TOKEN:?Environment variable GH_TOKEN must be set}"
@@ -22,8 +22,11 @@ main() {
       if [[ -d "$chart" ]]; then
         chart_name=$(yq eval .name "${chart}/Chart.yaml")
         chart_version=$(yq eval .version "${chart}/Chart.yaml")
+        set +euo pipefail
         gh release view $chart_name-$chart_version >/dev/null 2>&1
-        if [ $? -eq 0 ];then
+        local exit_code=$?
+        set -euo pipefail
+        if [ $exit_code -eq 0 ];then
           echo "Release '$chart_name-$chart_version' already exists...skipping."
         else
           echo "Creating release '$chart_name-$chart_version'"
